0cb1d931d62d30420330d0cc7d922cc92522bd97
Tomypv <tpv888@usal.es>
2025-02-25 20:02:10 +0100

    chore: install angular material, cdk and animations

--- prueba_Angular/src/app/app.config.ts
+++ prueba_Angular/src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app.routes';
 
+import { MatButtonModule } from '@angular/material/button';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    importProvidersFrom(MatButtonModule)
+  ]
 };
